6b01a6212f1be8d25eaa8ba845898f19dab50410
Giant8east <X2260883456@gmail.com>
2026-03-19 22:07:20 +0800

    目录行&目录项

--- app/src/main/java/com/example/handcraft_todolist/MainActivity.kt
+++ app/src/main/java/com/example/handcraft_todolist/MainActivity.kt
@@ -6,13 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,6 +30,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -32,6 +45,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.util.fastForEach
+import androidx.compose.ui.util.fastForEachIndexed
 import com.example.handcraft_todolist.ui.theme.HandCraft_TodoListTheme
 import com.example.handcraft_todolist.ui.util.px
 import com.example.handcraft_todolist.ui.util.textPx
@@ -71,6 +85,7 @@ fun TodoListScreen() {
                 .padding(innerPadding)
         ) {
             DateRow()
+            CategoryRow()
         }
     }
 }
@@ -182,5 +197,92 @@ fun DateItem(
             fontSize = 11.textPx(),
             color = if (dayInfo.isToday) Color.White else Color.Black
         )
+    }
+}
+
+//自定义形状
+val CustomShape = GenericShape { size, _ ->
+    // width / 22 height / 8
+    val (width, height) = size.width to size.height
+    moveTo(0f, height / 2) // 移动到左边垂直中心
+    // 左上角
+    cubicTo(
+        0f, height / 8, // 第一个控制点
+        0f, 0f, // 第二个控制点
+        width / 2, 0f // 终点
+    )
+    // 右上角
+    cubicTo(
+        width, 0f, // 第一个控制点
+        width, height / 8, // 第二个控制点
+        width, height / 2 // 终点
+    )
+    // 右下角
+    cubicTo(
+        width, height - height / 8, // 第一个控制点
+        width, height, // 第二个控制点
+        width / 2, height // 终点
+    )
+    // 左下角
+    cubicTo(
+        0f, height, // 第一个控制点
+        0f, height - height / 8, // 第二个控制点
+        0f, height / 2 // 终点
+    )
+    close()
+}
+
+@Composable
+fun CategoryRow(){
+    val categories = remember {
+        listOf("All", "To do", "In progress", "Completed")
+    }
+    var currentIndex by remember { mutableIntStateOf(0) }
+    Row(
+        modifier = Modifier
+            .padding(top = 32.px())
+            .fillMaxWidth()
+            .horizontalScroll(rememberScrollState()),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(12.px())
+    ) {
+        Spacer(Modifier.width(22.px()))
+        categories.fastForEachIndexed { index, category ->
+            val isSelected = index == currentIndex
+            CategoryItem(
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .height(34.px())
+                    .clip(CustomShape)
+                    .background(
+                        color = if (isSelected) Color(0xFF5F33E1) else Color(0xFFEDE8FF)
+                    )
+                    .clickable {
+                        currentIndex = index
+                    }
+                    .padding(vertical = 8.px(), horizontal = 24.px()),
+                category = category,
+                isSelected = isSelected
+            )
+        }
+        Spacer(Modifier.width(22.px()))
+    }
+}
+
+@Composable
+fun CategoryItem(
+    modifier: Modifier = Modifier,
+    category: String,
+    isSelected: Boolean
+){
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+       Text(
+           text = category,
+           fontWeight = if(isSelected) FontWeight.SemiBold else FontWeight.Normal,
+           color = if(isSelected) Color.White else Color(0xFF5F33E1)
+       )
     }
 }
